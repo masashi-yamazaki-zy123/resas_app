fbda3840ac26dc4e025c2b400121ac267aa568f6
5161806nttdocomo <5161806nttdocomo@gmail.com>
2024-11-29 01:52:31 +0000

    市区町村名を詳細画面へ渡せるようにした

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'city_detail_page.dart';
 
 class CityListPage extends StatelessWidget {
   const CityListPage({
@@ -35,7 +36,12 @@ class CityListPage extends StatelessWidget {
               subtitle: const Text('政令指定都市'),
               trailing: const Icon(Icons.navigate_next),
               onTap: () {
-                //TODO詳細画面に遷移する
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => CityDetailPage(
+                      city: city,),
+                  ),
+                );
               },
             ),
         ],
